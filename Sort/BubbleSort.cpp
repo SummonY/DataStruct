af202c60e726894f890086e01c48bd2f90e60d3a
Summon Yang <SummonYang1991@gmail.com>
2015-08-05 01:54:57 +0800

    Create BubbleSort.cpp file.

--- Sort/BubbleSort.cpp
+++ Sort/BubbleSort.cpp
@@ -29,7 +29,7 @@ void BundleSort(int A[], int n)
     int i, j, tmp;
 
     for (i = 0; i < n; ++i) {
-        for (j = 0; j < n - i; ++j) {
+        for (j = 0; j < n - i - 1; ++j) {
             if (A[j] > A[j + 1]) {
                 tmp = A[j];
                 A[j] = A[j + 1];
